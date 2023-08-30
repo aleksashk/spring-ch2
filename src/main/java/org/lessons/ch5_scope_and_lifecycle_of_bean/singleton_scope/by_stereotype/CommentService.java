edeec24253527fd2af3ff35d4844127c9042bb31
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-30 17:37:12 +0300

    scope singleton by stereotype with constructor injection

--- src/main/java/org/lessons/ch5_scope_and_lifecycle_of_bean/singleton_scope/by_stereotype/CommentService.java
+++ src/main/java/org/lessons/ch5_scope_and_lifecycle_of_bean/singleton_scope/by_stereotype/CommentService.java
@@ -1,13 +1,16 @@
 package org.lessons.ch5_scope_and_lifecycle_of_bean.singleton_scope.by_stereotype;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
 
-    @Autowired
-    private CommentRepository commentRepository;
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
 
     public CommentRepository getCommentRepository() {
         return this.commentRepository;
