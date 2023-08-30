01338021f04a14e27cbf9243d158e4b0462bc841
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-30 17:17:44 +0300

    with spring and change @Component to @Repository

--- src/main/java/org/lessons/ch4_abstraction/with_spring_and_qualifier/repositories/DBCommentRepository.java
+++ src/main/java/org/lessons/ch4_abstraction/with_spring_and_qualifier/repositories/DBCommentRepository.java
@@ -2,8 +2,9 @@ package org.lessons.ch4_abstraction.with_spring_and_qualifier.repositories;
 
 import org.lessons.ch4_abstraction.with_spring_and_qualifier.model.Comment;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class DBCommentRepository implements CommentRepository {
     @Override
     public void storeComment(Comment comment) {
