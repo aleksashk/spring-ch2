92b67dc4ec038c1f3eeb366a76105ac1831cdb88
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-30 19:28:43 +0300

    simple aop

---
+++ src/main/java/org/lessons/ch6_aop/simple_aspect/aspect/LoggingAspect.java
@@ -0,0 +1,25 @@
+package org.lessons.ch6_aop.simple_aspect.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.util.logging.Logger;
+
+@Aspect
+public class LoggingAspect {
+
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
+    @Around("execution(* org.lessons.ch6_aop.simple_aspect.service.*.*(..))")
+    public void log(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Method will execute");
+        joinPoint.proceed();
+        logger.info("Method executed");
+    }
+
+    public void setLogger(Logger logger) {
+        this.logger = logger;
+    }
+
+}
--- src/main/java/org/lessons/ch6_aop/simple_aspect/config/ProjectConfig.java
+++ src/main/java/org/lessons/ch6_aop/simple_aspect/config/ProjectConfig.java
@@ -1,10 +1,19 @@
 package org.lessons.ch6_aop.simple_aspect.config;
 
+import org.lessons.ch6_aop.simple_aspect.aspect.LoggingAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = "org.lessons.ch6_aop.simple_aspect")
+@EnableAspectJAutoProxy
 public class ProjectConfig {
+
+    @Bean
+    public LoggingAspect aspect() {
+        return new LoggingAspect();
+    }
 
 }
