7dbe65e0ab0cca7ab4a36c0c1daf138cf0b3f625
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-30 11:18:46 +0300

    getBean with program meth

---
+++ src/main/java/org/lessons/ch2/main/Dog.java
@@ -0,0 +1,9 @@
+package org.lessons.ch2.main;
+
+public class Dog {
+    private String voice;
+
+    public String bark() {
+        return "Wow";
+    }
+}
--- src/main/java/org/lessons/ch2/main/Main.java
+++ src/main/java/org/lessons/ch2/main/Main.java
@@ -3,21 +3,38 @@ package org.lessons.ch2.main;
 import org.lessons.ch2.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        String strHello = context.getBean(String.class);
-        System.out.println(strHello);
+//        String strHello = context.getBean(String.class);
+//        System.out.println(strHello);
+//
+//        Integer int10 = context.getBean(Integer.class);
+//        System.out.println(int10);
 
-        Integer int10 = context.getBean(Integer.class);
-        System.out.println(int10);
-
-        Parrot parrotJacklin = context.getBean("Robert",Parrot.class);
+        //с помощью @Bean
+        Parrot parrotJacklin = context.getBean("Robert", Parrot.class);
         System.out.println(parrotJacklin.getName());
 
+        //с помощью @Component и @ComponentScan(basePackage="org.lessons.ch2.main") и @PostConstruct
         Cat catBean = context.getBean(Cat.class);
         System.out.println(catBean.getName());
+
+        //Программный способ
+        Dog ourDog = new Dog();
+        Supplier<Dog> dogSupplier = () -> ourDog;
+
+        context.registerBean(
+                "Sharik",
+                Dog.class,
+                dogSupplier,
+                dog -> dog.setPrimary(true));
+
+        Dog sharik = context.getBean(Dog.class);
+        System.out.println(sharik.bark());
 
     }
 }
